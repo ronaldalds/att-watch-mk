92715942a9f2dc87d31150d2695c12dce2fc93f1
ralds <ronald.ralds@gmail.com>
2023-09-22 16:14:38 -0300

    mult thread

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ def read_item(item_id: int, Authorization: str = Header(None)):
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8003)
+    uvicorn.run("main:app", host="0.0.0.0", port=8002)
